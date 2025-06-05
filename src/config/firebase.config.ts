ed35d8e37a2eae0097c30cd0e2a3e95cf8793f37
momodou lamin jammeh <muhammadlaminjammeh@live.com>
2025-06-05 16:40:26 +0200

    Fix: configuring the redux toolkit x firebase addons (#1)

---
+++ src/config/firebase.config.ts
@@ -0,0 +1,15 @@
+// Firebase configuration:: Request the admin of this Project about the config parameter values,
+// in other to connect the main app configuration of this project to the firebase those parameter
+// values should be required
+//To Firebase
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyC6loFbxwBsq66e1sUmP0XrmIhjPxYw0fg",
+  authDomain: "assignments-apps.firebaseapp.com",
+  databaseURL: "https://assignments-apps-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "assignments-apps",
+  storageBucket: "assignments-apps.firebasestorage.app",
+  messagingSenderId: "841269924505",
+  appId: "1:841269924505:web:c71b31041375def811de08",
+  measurementId: "G-KDHR6TCQ18"
+};
